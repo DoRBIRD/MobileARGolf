4473e5d99cc1777296fd6c74c14b0ddf9a006694
Jonas <jonas@oja.de>
2017-11-22 14:50:24 +0100

    added movingobstacle

---
+++ Assets/MovingObstacle.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingObstacle : MonoBehaviour {
+    public Transform Point1;
+    public Transform Point2;
+    public GameObject Obstacle;
+    private float movingTime;
+    public float speed;
+    private float distance;
+    private float currentTime;
+    private float direction = 1;
+    // Use this for initialization
+
+    void Start () {
+        distance = (Point1.position - Point2.position).magnitude;
+        movingTime = distance / speed;
+        currentTime = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        currentTime += (direction * Time.deltaTime);
+        if (currentTime > movingTime || currentTime < 0 )
+            direction = -direction;
+
+        Vector3.ro
+
+        float perct = currentTime/movingTime;        
+        Obstacle.transform.position = Vector3.Lerp(Point1.position, Point2.position, perct);
+
+
+    }
+}
