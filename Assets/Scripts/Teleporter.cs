b6ce9e2e71f9bc74bab0ea4691e7969ef8a50c40
Julius Hinrichs <jhinrichs@stud.hs-bremen.de>
2017-11-22 16:04:21 +0100

    TeleporterPai

---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,47 @@
+﻿using Assets.Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+    public Teleporter OtherTeleporter;
+    private bool _ready = true;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Disable()
+    {
+        _ready = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        GolfPlayerController golfPlayer = other.gameObject.GetComponent<GolfPlayerController>();
+        if (!golfPlayer || !_ready) return;
+        OtherTeleporter.Disable();
+        //print(OtherTeleporter.transform.position);
+        //print(this.name);
+        print(OtherTeleporter.gameObject.transform.rotation.eulerAngles.y);
+        golfPlayer.GetComponent<Rigidbody>().velocity =
+            Quaternion.AngleAxis(OtherTeleporter.gameObject.transform.rotation.eulerAngles.y, Vector3.up) * golfPlayer.GetComponent<Rigidbody>().velocity;
+        golfPlayer.transform.position = OtherTeleporter.gameObject.transform.position;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        GolfPlayerController golfPlayer = other.gameObject.GetComponent<GolfPlayerController>();
+        if (!golfPlayer || _ready) return;
+        _ready = true;
+    }
+}
