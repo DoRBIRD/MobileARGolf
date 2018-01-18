b034f806c78b3733c09966c0017c5792f1cd83c8
Jonas <jonas@oja.de>
2018-01-18 16:30:25 +0100

    fix for inputbtn

---
+++ Assets/Scripts/ChangeInputMode.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class ChangeInputMode : MonoBehaviour {
+
+        private GolfPlayerController _golfPlayer;
+	    // Use this for initialization
+	    void Start () {
+		
+	    }
+	
+	    // Update is called once per frame
+	    void Update () {
+            GameObject go = GameObject.FindGameObjectWithTag("Player");
+            if (go)
+                _golfPlayer = go.GetComponent<GolfPlayerController>();
+        }
+
+
+        public void changeInputMode()
+        {
+            _golfPlayer.ToggleInputMode();
+        }
+    }
+}
